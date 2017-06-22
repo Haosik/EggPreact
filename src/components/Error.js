eb761adc5430a6835d99eb6dd3e5df029a488ed4
Haosik <noobbaster@gmail.com>
2017-06-22 16:31:16 +0300

    added 404 (Error route)

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+
+export default function Error() {
+    return (
+        <div>
+            <p>Sorry! 404</p>
+            <p><a href="/">Go to home page</a></p>
+        </div>
+    )
+}
